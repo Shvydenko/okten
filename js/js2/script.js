4a3915c555296f7cdab2959d4f2b721dc6402877
Shvydenko <yarykshved1@gmail.com>
2022-04-24 01:22:02 +0300

    js1 js2

---
+++ js/js2/script.js
@@ -0,0 +1,93 @@
+// - Є змінна х, якій ви надаєте довільне числове значення.
+//     Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
+
+// let x = -3;
+// if (x !== 0){
+//     document.write(`<div>"Вірно"</div>`);
+// }
+// else{
+//     document.write(`<div>"Невірно"</div>`);
+// }
+
+
+
+// - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
+// (в першу, другу, третю или четверту частину години).
+
+// let time = -10;
+//
+// if (time <= 14 && time >= 0){
+//     document.write(`<div>"First"</div>`);
+// }
+// else if (time >= 15 && time <= 29) {
+//     document.write(`<div>"Second"</div>`);
+// }
+// else if (time >= 30 && time <= 44) {
+//     document.write(`<div>"Third"</div>`);
+// }
+// else if (time >= 45 && time <= 59) {
+//     document.write(`<div>"Fourth"</div>`)
+// }
+// else console.log("Infinity");
+
+// - У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
+
+
+// let day = -10;
+// if (day <= 10 && day >=1 ){
+//     console.log("First part of month");
+// }
+// else if (day >=11 && day <= 20){
+//     console.log("Second part of month");
+// }
+// else if (day >=21 && day <= 31){
+//     console.log("Third part of month");
+// }
+// else console.log(undefined);
+
+
+// - Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається
+// інфа що заплановано на цей день (можна замість плану на день, назву дня англійською).
+
+
+// switch (4){
+//     case 1:
+//         console.log("Monday");
+//         break
+//     case 2:
+//         console.log("Tuesday");
+//         break
+//     case 3:
+//         console.log("Wednesday");
+//         break
+//     case 4:
+//         console.log("Thursday");
+//         break
+//     case 5:
+//         console.log("Friday");
+//         break
+//     case 6:
+//         console.log("Saturday");
+//         break
+//     case 7:
+//         console.log("Sunday");
+//         break
+//     default :
+//         console.log("Holiday");
+// }
+
+
+//     - Користувач вводить або має два числа.
+//     Потрібно знайти та вивести максимальне число з тих двох .
+//     Також потрібно врахувати коли введені рівні числа.
+
+
+// let x = 1100
+// let y = 110
+//
+// if (x > y ) {
+//     console.log("First number x")}
+// else if (x === y)
+//     {console.log("number x and y")}
+//     else (console.log("Second number y"))
+
